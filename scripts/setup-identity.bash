188c7967208d85fce646c7085c379668ec68406d
Ivan Ivanov <ivan.ivanov@agentsmutual.co.uk>
2018-05-03 17:17:32 +0300

    Add: OSX Sierra fix

--- scripts/setup-identity.bash
+++ scripts/setup-identity.bash
@@ -29,8 +29,8 @@ security list-keychains
 security import $APPLE_CERT -k $CUSTOM_KEYCHAIN -A
 security import $DECRYPTED_DEV_CERT -k $CUSTOM_KEYCHAIN -A -P "$KEYPASS"
 
-# Fix for OS X Sierra that hungs in the codesign step
-# security set-key-partition-list -S apple-tool:,apple: -s -k $SECURITY_PASSWORD $CUSTOM_KEYCHAIN > /dev/null
+#Fix for OS X Sierra that hungs in the codesign step
+security set-key-partition-list -S apple-tool:,apple: -s -k $KEYPASS $CUSTOM_KEYCHAIN > /dev/null
 
 #import provisioning profile
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
