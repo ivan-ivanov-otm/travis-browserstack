004994478b5a2c9d4e6c7023ad9e336ad25833b9
Ivan Ivanov <ivan.ivanov@agentsmutual.co.uk>
2018-05-09 15:52:54 +0300

    FIX: Upload script

--- scripts/upload-ipa.bash
+++ scripts/upload-ipa.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE_PATH="./build/Products/IPA"
+FILE_PATH="$BUILD_DIR$IPA_FILE_NAME"
 UPLOAD_PATH="https://api-cloud.browserstack.com/app-automate/upload" 
 
 curl -u "$BROWSERSTACK_USER:$BROWSERSTACK_PASS" -X POST $UPLOAD_PATH "file=@$FILE_PATH"
