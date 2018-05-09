bf40b99e7ab123267a9d3215203fbd1dc00ce653
Ivan Ivanov <ivan.ivanov@agentsmutual.co.uk>
2018-05-09 16:29:13 +0300

    Add: Writing the upload call to file

--- scripts/upload-ipa.bash
+++ scripts/upload-ipa.bash
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 FILE_PATH="./build/Products/IPA/TravisBrowserstack.ipa"
-UPLOAD_PATH="https://api-cloud.browserstack.com/app-automate/upload" 
+UPLOAD_PATH="https://api-cloud.browserstack.com/app-automate/upload"
+CONFIG_FILE_PATH="./scripts/app_url.json"
 
-curl -u "$BROWSERSTACK_USER:$BROWSERSTACK_PASS" -X POST $UPLOAD_PATH "file=@$FILE_PATH"
+curl -u "$BROWSERSTACK_USER:$BROWSERSTACK_PASS" -X POST $UPLOAD_PATH -F "file=@$FILE_PATH" -o CONFIG_FILE_PATH
+
+cat CONFIG_FILE_PATH
