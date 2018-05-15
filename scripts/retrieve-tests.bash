efa1e9800d160f06249f5bd1a607eb495bcef2f2
Ivan Ivanov <ivan.ivanov@agentsmutual.co.uk>
2018-05-15 15:38:28 +0300

    Add: Cloning the test via https instead of ssh

--- scripts/retrieve-tests.bash
+++ scripts/retrieve-tests.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 TESTS_PATH="$HOME/tests/"
-REPOSITORY_URL="git@github.com:ivan-ivanov-otm/ui-tests.git"
+REPOSITORY_URL="https://github.com/ivan-ivanov-otm/ui-tests.git"
 
 git clone $REPOSITORY_URL $TESTS_PATH
