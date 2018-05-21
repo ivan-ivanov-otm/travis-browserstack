60cacc839ffa93e98b6fcf9520c09be1e4d80e1c
Ivan Ivanov <ivan.ivanov@agentsmutual.co.uk>
2018-05-21 10:18:03 +0300

    Add: Logging to help debugging

--- scripts/run-tests.bash
+++ scripts/run-tests.bash
@@ -3,4 +3,9 @@
 APP_ENTRY="$HOME/tests/app.js"
 BINARY_PATH="./build/Products/IPA/TravisBrowserstack.ipa"
 
+echo $APP_ENTRY
+echo $BINARY_PATH
+echo $BROWSERSTACK_USER
+echo $BROWSERSTACK_PASS
+
 node $APP_ENTRY -u $BROWSERSTACK_USER -p $BROWSERSTACK_PASS -f $BINARY_PATH
